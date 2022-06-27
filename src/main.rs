dc341f6e41b63a3806895b71fa9c3951917ce20a
Martin Olivier <martin.olivier@live.fr>
2022-06-27 13:51:14 +0200

    feat: CLI
    Signed-off-by: Martin Olivier <martin.olivier@live.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod parser;
+
+use pcap::Device;
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = parser::Args::parse();
+
+    let dev = match args.device {
+        Some(dev) => Device::from(dev.as_str()),
+        None      => Device::lookup().expect("device lookup failed"),
+    };
+
+    println!("Using device \"{}\"", dev.name);
+
+    let mut cap = pcap::Capture::from_device(dev)
+        .unwrap()
+        .immediate_mode(true)
+        .open()
+        .unwrap();
+
+    let mut savefile = match args.output {
+        Some(file) => Some(cap.savefile(file).unwrap()),
+        None       => None,
+    };
+
+    while let Ok(packet) = cap.next() {
+        if args.verbose {
+            println!("received packet! {:?}", packet);
+        }
+        if savefile.is_some() {
+            savefile.as_mut().unwrap().write(&packet);
+        }
+    }
 }
