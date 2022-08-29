fb2b42d45f3b670e1862c892648e027d09d45932
Martin Olivier <martin.olivier@live.fr>
2022-08-29 13:11:11 +0200

    feat: working sniffer and cli
    Signed-off-by: Martin Olivier <martin.olivier@live.fr>

---
+++ src/args.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+pub struct Args {
+    /// IP adress of the grpc server
+    #[clap(short, long, value_parser)]
+    pub ip: Option<String>,
+
+    /// Port of the grpc server
+    #[clap(short, long, value_parser, default_value_t = 50051)]
+    pub port: u16,
+
+    /// Set pcap output file if you want to save the captured packets
+    #[clap(short, long, value_parser)]
+    pub output: Option<String>,
+
+    /// If provided, the captured packets will be displayed in the shell
+    #[clap(short, long, value_parser, default_value_t = false)]
+    pub verbose: bool,
+
+    /// If provided, the sniffer will use the provided interface
+    #[clap(short, long, value_parser)]
+    pub device: Option<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+
+use args::Args;
+use clap::Parser;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let dev = match args.device {
+        Some(dev) => pcap::Device::from(dev.as_str()),
+        None => pcap::Device::lookup()?,
+    };
+
+    println!("Using device \"{}\"", dev.name);
+
+    let mut cap = pcap::Capture::from_device(dev)?
+        .immediate_mode(true)
+        .open()?;
+
+    let mut savefile = match args.output {
+        Some(file) => Some(cap.savefile(file)?),
+        None => None,
+    };
+
+    while let Ok(packet) = cap.next() {
+        if args.verbose {
+            println!("received packet! {:?}", packet);
+        }
+        if savefile.is_some() {
+            savefile.as_mut().unwrap().write(&packet);
+        }
+    }
+    Ok(())
 }
