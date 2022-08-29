a078833a7811e7ef5a39871537a81745b88ce931
Martin Olivier <martin.olivier@live.fr>
2022-08-29 13:50:41 +0200

    fix: switched cli args ip and port to url
    Signed-off-by: Martin Olivier <martin.olivier@live.fr>

--- src/args.rs
+++ src/args.rs
@@ -3,23 +3,19 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
 pub struct Args {
-    /// IP adress of the grpc server
+    /// URL of the grpc server to send the pcap data stream
     #[clap(short, long, value_parser)]
-    pub ip: Option<String>,
-
-    /// Port of the grpc server
-    #[clap(short, long, value_parser, default_value_t = 50051)]
-    pub port: u16,
+    pub url: Option<String>,
 
     /// Path to a pcap output file where to save captured packets
     #[clap(short, long, value_parser)]
     pub output: Option<String>,
 
-    /// If provided, the captured packets will be displayed in the shell
+    /// The captured packets will be displayed in the shell
     #[clap(short, long, value_parser, default_value_t = false)]
     pub verbose: bool,
 
-    /// If provided, the sniffer will use the provided interface instead of the default one
+    /// The provided network device will be used instead of the default one
     #[clap(short, long, value_parser)]
     pub device: Option<String>,
 }
